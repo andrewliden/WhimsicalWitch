59ac7445808336019e574324aadcfd74cc40eb3d
andrewliden <47042544+andrewliden@users.noreply.github.com>
2020-04-09 20:19:44 -0400

    Started testing path following.  works so far

--- dev/scenes/char_playable/witch_camera_rig.gd
+++ dev/scenes/char_playable/witch_camera_rig.gd
@@ -2,6 +2,8 @@ extends Spatial
 
 const CAMERA_TRACK_STRENGTH = 0.5
 
+var trackSpeed = 10
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,7 +12,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	track_witch()
-	
+	follow_path(delta)
+
 func track_witch():
 	$Camera.translation.x = $witch_move_rig.translation.x * CAMERA_TRACK_STRENGTH
 	$Camera.translation.y = $witch_move_rig.translation.y * CAMERA_TRACK_STRENGTH
+
+func follow_path(delta):
+	#If the offset attribute exists on the parent, follow the path
+	if get_parent().offset != null:
+		get_parent().offset += trackSpeed * delta
