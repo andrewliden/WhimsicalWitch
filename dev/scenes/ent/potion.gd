001adfb9643c5e218742325aba19a07088e1cd65
andrewliden <47042544+andrewliden@users.noreply.github.com>
2020-04-15 16:38:09 -0400

    Added some sounds, started a new enemy type

---
+++ dev/scenes/ent/potion.gd
@@ -0,0 +1,32 @@
+extends RigidBody
+
+
+const THROW_FORCE = Vector3(0, 10, 40)
+var explosion
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	explosion = preload("res://scenes/ent/potion_explosion.tscn")
+	apply_impulse(Vector3(0.1,1,0), THROW_FORCE)
+	add_to_group("projectiles")
+	add_to_group("potions")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#the potion will explode if the user right clicks again, or it hits an object
+	if Input.is_action_just_pressed("gameplay_alt_fire"):
+		explode()
+	var collisions = get_colliding_bodies()
+	if collisions.size() > 0:
+		explode()
+
+func explode():
+	var explosionInstance = explosion.instance()
+	get_tree().get_root().add_child(explosionInstance)
+	explosionInstance.global_transform = global_transform
+	queue_free()
+
+#The timer expiring also causes the potion to explode.
+func _on_DestroyTimer_timeout():
+	explode()
