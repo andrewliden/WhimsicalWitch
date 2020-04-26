58c52c81141d911f48a44bf173598d83fe621032
andrewliden <47042544+andrewliden@users.noreply.github.com>
2020-04-26 15:33:07 -0400

    Final game version.

--- dev/scenes/control/Number.gd
+++ dev/scenes/control/Number.gd
@@ -13,6 +13,6 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("gameplay_fire"):
-		get_tree().change_scene("res://scenes/control/IntroSplash.tscn")
+		var _err = get_tree().change_scene("res://scenes/control/IntroSplash.tscn")
